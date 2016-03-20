eed05c1f17a729e8ac7e5bb5761e367551230544
Robert Szekely <szekelyrobert1995@gmail.com>
2016-03-20 13:10:35 +0200

    added facePath for drawing a circle which is the face of the smiley

---
+++ Happiness/FaceView.swift
@@ -0,0 +1,44 @@
+//
+//  FaceView.swift
+//  Happiness
+//
+//  Created by Robert Szekely on 20/03/16.
+//  Copyright © 2016 Robert Szekely. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+    
+    var lineWidth: CGFloat = 3 { didSet { setNeedsDisplay() } }
+    var color: UIColor = UIColor.blueColor() { didSet { setNeedsDisplay() } }
+    var scale: CGFloat = 0.90 { didSet { setNeedsDisplay() } }
+    
+    var faceCenter: CGPoint {
+        return convertPoint(center, fromView: superview)
+    }
+    
+    var faceRadius: CGFloat {
+        return min(bounds.size.width, bounds.size.height) / 2 * scale
+    }
+    
+    
+    override func drawRect(rect: CGRect) {
+        let facePath = UIBezierPath(
+            arcCenter: faceCenter,
+            radius: faceRadius,
+            startAngle: 0,
+            endAngle: CGFloat(2*M_PI),
+            clockwise: true
+        )
+        facePath.lineWidth = lineWidth
+        color.set()
+        facePath.stroke()
+
+    }
+    
+    
+    
+
+
+}
---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Robert Szekely on 20/03/16.
+//  Copyright © 2016 Robert Szekely. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+    
+
+}
